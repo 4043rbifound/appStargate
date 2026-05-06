95caad39c869c33282994a349853e13330bced27
MomoaKa47 <chatibimohamed20@gmail.com>
2026-05-06 09:47:03 +0200

    Ajout des imports dans dev

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Sql;
+using BCrypt;
 using System.Drawing;
 using System.Linq;
 using System.Text;
