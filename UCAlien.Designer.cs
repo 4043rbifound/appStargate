4c3359554bbf223a263f310f31493c9885a806ed
shinobyTN <aminebouzaiene2006@gmail.com>
2026-05-28 09:53:21 +0100

    réglage des param sur UCAliens pour l'affichage

--- UCAlien.Designer.cs
+++ UCAlien.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class UCAlien
     {
-        /// <summary> 
-        /// Variable nécessaire au concepteur.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Nettoyage des ressources utilisées.
-        /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Code généré par le Concepteur de composants
-
-        /// <summary> 
-        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
-        /// le contenu de cette méthode avec l'éditeur de code.
-        /// </summary>
         private void InitializeComponent()
         {
             this.pbAlien = new System.Windows.Forms.PictureBox();
@@ -34,63 +21,54 @@
             this.lblType = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbAlien)).BeginInit();
             this.SuspendLayout();
-            // 
+
             // pbAlien
-            // 
-            this.pbAlien.Location = new System.Drawing.Point(20, 12);
-            this.pbAlien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbAlien.Location = new System.Drawing.Point(15, 10);
             this.pbAlien.Name = "pbAlien";
-            this.pbAlien.Size = new System.Drawing.Size(160, 148);
+            this.pbAlien.Size = new System.Drawing.Size(120, 120);
+            this.pbAlien.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbAlien.TabIndex = 0;
             this.pbAlien.TabStop = false;
-            // 
+
             // lblNom
-            // 
-            this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(16, 166);
-            this.lblNom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNom.AutoSize = false; // ← false pour éviter le décalage
+            this.lblNom.Location = new System.Drawing.Point(5, 135);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(44, 16);
+            this.lblNom.Size = new System.Drawing.Size(140, 20);
             this.lblNom.TabIndex = 1;
-            this.lblNom.Text = "label1";
-            // 
+            this.lblNom.Text = "";
+            this.lblNom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNom.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+
             // lblCouleur
-            // 
-            this.lblCouleur.AutoSize = true;
-            this.lblCouleur.Location = new System.Drawing.Point(16, 194);
-            this.lblCouleur.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCouleur.AutoSize = false; // ← false
+            this.lblCouleur.Location = new System.Drawing.Point(5, 158);
             this.lblCouleur.Name = "lblCouleur";
-            this.lblCouleur.Size = new System.Drawing.Size(44, 16);
+            this.lblCouleur.Size = new System.Drawing.Size(140, 20);
             this.lblCouleur.TabIndex = 2;
-            this.lblCouleur.Text = "label1";
-            // 
+            this.lblCouleur.Text = "";
+            this.lblCouleur.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // lblType
-            // 
-            this.lblType.AutoSize = true;
-            this.lblType.Location = new System.Drawing.Point(16, 219);
-            this.lblType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblType.AutoSize = false; // ← false
+            this.lblType.Location = new System.Drawing.Point(5, 178);
             this.lblType.Name = "lblType";
-            this.lblType.Size = new System.Drawing.Size(44, 16);
+            this.lblType.Size = new System.Drawing.Size(140, 20);
             this.lblType.TabIndex = 3;
-            this.lblType.Text = "label2";
-            // 
+            this.lblType.Text = "";
+            this.lblType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // UCAlien
-            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.lblType);
             this.Controls.Add(this.lblCouleur);
             this.Controls.Add(this.lblNom);
             this.Controls.Add(this.pbAlien);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UCAlien";
-            this.Size = new System.Drawing.Size(200, 246);
+            this.Size = new System.Drawing.Size(150, 200);
             ((System.ComponentModel.ISupportInitialize)(this.pbAlien)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
-
-        #endregion
 
         private System.Windows.Forms.PictureBox pbAlien;
         private System.Windows.Forms.Label lblNom;
