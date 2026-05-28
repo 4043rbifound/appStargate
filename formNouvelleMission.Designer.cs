9f1d997968e2eb8583ac1e03b73d55aa82336243
Mohamed.B <lespredateurrrr@gmail.com>
2026-05-28 08:34:54 +0200

    ajout lbl numero mission

--- formNouvelleMission.Designer.cs
+++ formNouvelleMission.Designer.cs
@@ -29,16 +29,22 @@
         private void InitializeComponent()
         {
             this.grpNouvelleMission = new System.Windows.Forms.GroupBox();
-            this.lblChoixPlanete = new System.Windows.Forms.Label();
-            this.cboPlanete = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumMission = new System.Windows.Forms.Label();
+            this.lblNomDeMission = new System.Windows.Forms.Label();
+            this.lblMission = new System.Windows.Forms.Label();
             this.btnValiderPlanete = new System.Windows.Forms.Button();
-            this.lblNom = new System.Windows.Forms.Label();
+            this.cboPlanete = new System.Windows.Forms.ComboBox();
+            this.lblChoixPlanete = new System.Windows.Forms.Label();
             this.grpNouvelleMission.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpNouvelleMission
             // 
-            this.grpNouvelleMission.Controls.Add(this.lblNom);
+            this.grpNouvelleMission.Controls.Add(this.label1);
+            this.grpNouvelleMission.Controls.Add(this.lblNumMission);
+            this.grpNouvelleMission.Controls.Add(this.lblNomDeMission);
+            this.grpNouvelleMission.Controls.Add(this.lblMission);
             this.grpNouvelleMission.Controls.Add(this.btnValiderPlanete);
             this.grpNouvelleMission.Controls.Add(this.cboPlanete);
             this.grpNouvelleMission.Controls.Add(this.lblChoixPlanete);
@@ -50,22 +56,42 @@
             this.grpNouvelleMission.TabStop = false;
             this.grpNouvelleMission.Text = "Nouvelle mission";
             // 
-            // lblChoixPlanete
+            // label1
             // 
-            this.lblChoixPlanete.AutoSize = true;
-            this.lblChoixPlanete.Location = new System.Drawing.Point(40, 92);
-            this.lblChoixPlanete.Name = "lblChoixPlanete";
-            this.lblChoixPlanete.Size = new System.Drawing.Size(297, 32);
-            this.lblChoixPlanete.TabIndex = 0;
-            this.lblChoixPlanete.Text = "1 - Choix de la planète";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "2 - Choix de la planète";
             // 
-            // cboPlanete
+            // lblNumMission
             // 
-            this.cboPlanete.FormattingEnabled = true;
-            this.cboPlanete.Location = new System.Drawing.Point(390, 92);
-            this.cboPlanete.Name = "cboPlanete";
-            this.cboPlanete.Size = new System.Drawing.Size(385, 40);
-            this.cboPlanete.TabIndex = 1;
+            this.lblNumMission.AutoSize = true;
+            this.lblNumMission.Location = new System.Drawing.Point(668, 207);
+            this.lblNumMission.Name = "lblNumMission";
+            this.lblNumMission.Size = new System.Drawing.Size(30, 32);
+            this.lblNumMission.TabIndex = 5;
+            this.lblNumMission.Text = "0";
+            // 
+            // lblNomDeMission
+            // 
+            this.lblNomDeMission.AutoSize = true;
+            this.lblNomDeMission.Location = new System.Drawing.Point(384, 207);
+            this.lblNomDeMission.Name = "lblNomDeMission";
+            this.lblNomDeMission.Size = new System.Drawing.Size(143, 32);
+            this.lblNomDeMission.TabIndex = 4;
+            this.lblNomDeMission.Text = "PLANETE";
+            // 
+            // lblMission
+            // 
+            this.lblMission.AutoSize = true;
+            this.lblMission.Location = new System.Drawing.Point(40, 207);
+            this.lblMission.Name = "lblMission";
+            this.lblMission.Size = new System.Drawing.Size(216, 32);
+            this.lblMission.TabIndex = 3;
+            this.lblMission.Text = "Nom de mission";
+            this.lblMission.Click += new System.EventHandler(this.lblNom_Click);
             // 
             // btnValiderPlanete
             // 
@@ -76,14 +102,23 @@
             this.btnValiderPlanete.Text = "Valider";
             this.btnValiderPlanete.UseVisualStyleBackColor = true;
             // 
-            // lblNom
+            // cboPlanete
             // 
-            this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(40, 273);
-            this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(92, 32);
-            this.lblNom.TabIndex = 3;
-            this.lblNom.Text = "label1";
+            this.cboPlanete.FormattingEnabled = true;
+            this.cboPlanete.Location = new System.Drawing.Point(390, 92);
+            this.cboPlanete.Name = "cboPlanete";
+            this.cboPlanete.Size = new System.Drawing.Size(385, 40);
+            this.cboPlanete.TabIndex = 1;
+            this.cboPlanete.SelectedIndexChanged += new System.EventHandler(this.cboPlanete_SelectedIndexChanged);
+            // 
+            // lblChoixPlanete
+            // 
+            this.lblChoixPlanete.AutoSize = true;
+            this.lblChoixPlanete.Location = new System.Drawing.Point(40, 92);
+            this.lblChoixPlanete.Name = "lblChoixPlanete";
+            this.lblChoixPlanete.Size = new System.Drawing.Size(297, 32);
+            this.lblChoixPlanete.TabIndex = 0;
+            this.lblChoixPlanete.Text = "1 - Choix de la planète";
             // 
             // formNouvelleMission
             // 
@@ -103,9 +138,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grpNouvelleMission;
-        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblMission;
         private System.Windows.Forms.Button btnValiderPlanete;
         private System.Windows.Forms.ComboBox cboPlanete;
         private System.Windows.Forms.Label lblChoixPlanete;
+        private System.Windows.Forms.Label lblNumMission;
+        private System.Windows.Forms.Label lblNomDeMission;
+        private System.Windows.Forms.Label label1;
     }
 }
