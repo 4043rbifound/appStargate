5932ef27c37303a3b328422e69e80bfd644f3d58
Admin <Admin@SHINOBY-DESKTOP>
2026-06-05 17:21:40 +0200

    .

--- FormStats.Designer.cs
+++ FormStats.Designer.cs
@@ -76,7 +76,7 @@
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(436, 415);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Membres communs";
@@ -125,7 +125,7 @@
             this.tabPage2.Controls.Add(this.rtbMissionsGrandes);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(436, 415);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Missions > 10";
@@ -151,7 +151,7 @@
             this.tabPage3.Controls.Add(this.rtbMissionsPlanete);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage3.Size = new System.Drawing.Size(436, 415);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Missions par planète";
@@ -177,7 +177,7 @@
             this.tabPage4.Controls.Add(this.rtbDepensesMax);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage4.Size = new System.Drawing.Size(436, 415);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Dépenses max";
@@ -205,7 +205,7 @@
             this.tabPage5.Controls.Add(this.label2);
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage5.Size = new System.Drawing.Size(436, 415);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Informateurs";
