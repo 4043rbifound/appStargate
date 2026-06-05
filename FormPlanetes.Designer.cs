321cdb181e4dd56f9cd22075af6da4ed75c6d82b
Admin <Admin@SHINOBY-DESKTOP>
2026-06-05 16:59:27 +0200

    .

--- FormPlanetes.Designer.cs
+++ FormPlanetes.Designer.cs
@@ -34,6 +34,7 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.flpPlanetes = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
+            this.chartRaces = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lbMissions = new System.Windows.Forms.ListBox();
@@ -41,22 +42,21 @@
             this.lblTitrePlanete = new System.Windows.Forms.Label();
             this.pnlNotif = new System.Windows.Forms.Panel();
             this.lblNotif = new System.Windows.Forms.Label();
-            this.chartRaces = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.flpPlanetes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRaces)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRaces)).BeginInit();
             this.pnlNotif.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chartRaces)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -71,8 +71,9 @@
             this.splitContainer1.Panel2.Controls.Add(this.lbMissions);
             this.splitContainer1.Panel2.Controls.Add(this.dgvRaces);
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(972, 654);
-            this.splitContainer1.SplitterDistance = 321;
+            this.splitContainer1.Size = new System.Drawing.Size(729, 531);
+            this.splitContainer1.SplitterDistance = 240;
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 0;
             // 
             // flpPlanetes
@@ -81,36 +82,56 @@
             this.flpPlanetes.Controls.Add(this.button1);
             this.flpPlanetes.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flpPlanetes.Location = new System.Drawing.Point(0, 0);
-            this.flpPlanetes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flpPlanetes.Margin = new System.Windows.Forms.Padding(2);
             this.flpPlanetes.Name = "flpPlanetes";
-            this.flpPlanetes.Size = new System.Drawing.Size(321, 654);
+            this.flpPlanetes.Size = new System.Drawing.Size(240, 531);
             this.flpPlanetes.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 2);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 98);
+            this.button1.Size = new System.Drawing.Size(56, 80);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // chartRaces
+            // 
+            this.chartRaces.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chartRaces.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRaces.Legends.Add(legend1);
+            this.chartRaces.Location = new System.Drawing.Point(14, 297);
+            this.chartRaces.Margin = new System.Windows.Forms.Padding(2);
+            this.chartRaces.Name = "chartRaces";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartRaces.Series.Add(series1);
+            this.chartRaces.Size = new System.Drawing.Size(284, 224);
+            this.chartRaces.TabIndex = 5;
+            this.chartRaces.Text = "chart1";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 197);
+            this.label2.Location = new System.Drawing.Point(11, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.Size = new System.Drawing.Size(106, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Missions effectuées :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Races présentes :";
             // 
@@ -118,31 +139,31 @@
             // 
             this.lbMissions.FormattingEnabled = true;
             this.lbMissions.HorizontalScrollbar = true;
-            this.lbMissions.ItemHeight = 16;
-            this.lbMissions.Location = new System.Drawing.Point(18, 213);
-            this.lbMissions.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbMissions.Location = new System.Drawing.Point(14, 173);
+            this.lbMissions.Margin = new System.Windows.Forms.Padding(2);
             this.lbMissions.Name = "lbMissions";
-            this.lbMissions.Size = new System.Drawing.Size(378, 148);
+            this.lbMissions.Size = new System.Drawing.Size(284, 121);
             this.lbMissions.TabIndex = 2;
             // 
             // dgvRaces
             // 
             this.dgvRaces.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRaces.Location = new System.Drawing.Point(18, 45);
-            this.dgvRaces.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvRaces.Location = new System.Drawing.Point(14, 37);
+            this.dgvRaces.Margin = new System.Windows.Forms.Padding(2);
             this.dgvRaces.Name = "dgvRaces";
             this.dgvRaces.RowHeadersWidth = 51;
             this.dgvRaces.RowTemplate.Height = 24;
-            this.dgvRaces.Size = new System.Drawing.Size(263, 150);
+            this.dgvRaces.Size = new System.Drawing.Size(197, 122);
             this.dgvRaces.TabIndex = 1;
             this.dgvRaces.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRaces_CellContentClick);
             // 
             // lblTitrePlanete
             // 
             this.lblTitrePlanete.AutoSize = true;
-            this.lblTitrePlanete.Location = new System.Drawing.Point(337, 9);
+            this.lblTitrePlanete.Location = new System.Drawing.Point(253, 7);
+            this.lblTitrePlanete.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTitrePlanete.Name = "lblTitrePlanete";
-            this.lblTitrePlanete.Size = new System.Drawing.Size(44, 16);
+            this.lblTitrePlanete.Size = new System.Drawing.Size(35, 13);
             this.lblTitrePlanete.TabIndex = 0;
             this.lblTitrePlanete.Text = "label1";
             // 
@@ -152,10 +173,10 @@
             this.pnlNotif.Controls.Add(this.lblNotif);
             this.pnlNotif.Controls.Add(this.lblTitrePlanete);
             this.pnlNotif.ForeColor = System.Drawing.Color.Snow;
-            this.pnlNotif.Location = new System.Drawing.Point(3, 0);
-            this.pnlNotif.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlNotif.Location = new System.Drawing.Point(2, 0);
+            this.pnlNotif.Margin = new System.Windows.Forms.Padding(2);
             this.pnlNotif.Name = "pnlNotif";
-            this.pnlNotif.Size = new System.Drawing.Size(393, 25);
+            this.pnlNotif.Size = new System.Drawing.Size(295, 20);
             this.pnlNotif.TabIndex = 1;
             this.pnlNotif.Visible = false;
             // 
@@ -165,37 +186,21 @@
             this.lblNotif.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblNotif.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNotif.Location = new System.Drawing.Point(0, 0);
+            this.lblNotif.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNotif.Name = "lblNotif";
-            this.lblNotif.Size = new System.Drawing.Size(61, 24);
+            this.lblNotif.Size = new System.Drawing.Size(51, 19);
             this.lblNotif.TabIndex = 0;
             this.lblNotif.Text = "label3";
             this.lblNotif.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // chartRaces
-            // 
-            this.chartRaces.BackColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            this.chartRaces.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartRaces.Legends.Add(legend1);
-            this.chartRaces.Location = new System.Drawing.Point(18, 366);
-            this.chartRaces.Name = "chartRaces";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartRaces.Series.Add(series1);
-            this.chartRaces.Size = new System.Drawing.Size(378, 276);
-            this.chartRaces.TabIndex = 5;
-            this.chartRaces.Text = "chart1";
-            // 
             // FormPlanetes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 654);
+            this.ClientSize = new System.Drawing.Size(729, 531);
             this.Controls.Add(this.pnlNotif);
             this.Controls.Add(this.splitContainer1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormPlanetes";
             this.Text = "FormPlanetes";
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -204,10 +209,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.flpPlanetes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartRaces)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRaces)).EndInit();
             this.pnlNotif.ResumeLayout(false);
             this.pnlNotif.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chartRaces)).EndInit();
             this.ResumeLayout(false);
 
         }
